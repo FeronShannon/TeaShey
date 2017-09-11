7bc184c62280ab59f929cea3251b0eaf6f7c9438
audrey-g <guitteaud.audrey@gmail.com>
2017-09-11 12:06:41 +0200

    Mise en commentaire index_dev.php ; ajout connexion BD dans prod.php

--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,16 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+// connexion base de donnees
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+            'driver'    => 'pdo_mysql',
+            'host'      => 'localhost',
+            'dbname'    => 'teashey',
+            'user'      => 'root',
+            'password'  => '',
+            'charset'   => 'utf8mb4',
+    ),
+));
