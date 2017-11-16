d39d61798f002758b214b64e39b4daf02a50f030
FeronShannon <shannpesca@gmail.com>
2017-11-16 17:17:50 +0100

    pour choix mise en place du slider

--- templates/section-slider.php
+++ templates/section-slider.php
@@ -3,9 +3,9 @@
 				<div class="c_slider"></div>
 				<div class="slider">
 					<figure>
-						<img src="img/dummy-640x310-1.jpg" alt="" width="640" height="310" />
+						<img src="/assets/img/dummy-640x310-1.jpg" alt="" width="640" height="310" />
 						<figcaption>Sélection du moment</figcaption>
-
+						<h6>3</h6>
 					</figure><!--
 					--><figure>
 						<img src="img/dummy-640x310-2.jpg" alt="" width="640" height="310" />
@@ -25,3 +25,73 @@
 				
 			<span id="timeline"></span>
 		</section>
+
+		<section id="diaporama" accueil>
+
+					<div id="big-slider">
+
+						<div>
+							<figure>
+						     	<img src="/assets/img/big-diapo/1.jpg" alt="" width="640" height="310" />
+						     	<figcaption>Sélection du moment</figcaption>
+					    	</figure>
+						</div>
+
+						<div>
+							<figure>
+						     	<img src="/assets/img/big-diapo/3.jpg" />
+						     	<figcaption>
+						     		<h2>3</h2>
+						     	</figcaption>
+					    	</figure>
+						</div>
+
+						<div>
+							<figure>
+						     	<img src="/assets/img/big-diapo/4.jpg" />
+						     	<figcaption>
+						     		<h2>4</h2>
+						     	</figcaption>
+					    	</figure>
+						</div>
+
+						<div>
+							<figure>
+						     	<img src="/assets/img/big-diapo/5.jpg" />
+						     	<figcaption>
+						     		<h2>5</h2>
+						     	</figcaption>
+					    	</figure>
+						</div>
+
+						<div>
+							<figure>
+						     	<img src="/assets/img/big-diapo/7.jpg" />
+						     	<figcaption>
+						     		<h2>7</h2>
+						     	</figcaption>
+					    	</figure>
+						</div>
+
+						<div>
+							<figure>
+						     	<img src="/assets/img/big-diapo/8.jpg" />
+						     	<figcaption>
+						     		<h2>8</h2>
+						     	</figcaption>
+					    	</figure>
+						</div>
+
+						<div>
+							<figure>
+						     	<img src="/assets/img/big-diapo/9.jpg" />
+						     	<figcaption>
+						     		<h2>9</h2>
+						     	</figcaption>
+					    	</figure>
+						</div>
+
+
+					</div>
+					
+				</section>
